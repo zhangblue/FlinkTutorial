1cc109d24e2c0913620f9a7c5d16e0c67e0996de
zhangblue <zhangdi013@sina.com>
2020-07-05 19:55:42 +0800

    add sink demo

---
+++ src/main/scala/com/zhangblue/sink/SinkKafkaDemo.scala
@@ -0,0 +1,60 @@
+package com.zhangblue.sink
+
+import java.util.Properties
+
+import com.alibaba.fastjson.JSONObject
+import com.zhangblue.entity.TemperatureSensor
+import org.apache.flink.api.common.serialization.{DeserializationSchema, SimpleStringSchema}
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.internal.FlinkKafkaProducer
+import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer011, FlinkKafkaProducer011}
+
+/**
+ * Flink流处理写入kafka sink demo
+ *
+ * @author di.zhang
+ * @date 2020/7/5
+ * @time 17:42
+ **/
+object SinkKafkaDemo {
+  def main(args: Array[String]): Unit = {
+    //需求，求出传感器迄今为止探索到的最高温度的传感器数据
+    //1. 环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    //2. 读取kafka源，根据传感器的id进行分组，求每组中最大的温度值，并显示结果
+    val topic = "my-topic"
+    val valueDeserializationSchema: DeserializationSchema[String] = new SimpleStringSchema()
+    val props: Properties = new Properties()
+    props.load(this.getClass.getClassLoader.getResourceAsStream("kafka/localhost-consumer.properties"))
+    val kafkaSource = env.addSource(new FlinkKafkaConsumer011[String](topic, valueDeserializationSchema, props))
+
+    val srcDataStream: DataStream[TemperatureSensor] = kafkaSource.filter(_.trim.nonEmpty).map(fun = lineData => {
+      val arr = lineData.split(",")
+      val id = arr(0).trim
+      val timestamp = arr(1).trim.toLong
+      val name = arr(2).trim
+      val temperature = arr(3).trim.toDouble
+      val location = arr(4).trim
+      TemperatureSensor(id, timestamp, name, temperature, location)
+    }).keyBy("id").maxBy("temperature")
+
+
+    val producerConfig:Properties = new Properties();
+    producerConfig.load(this.getClass.getClassLoader.getResourceAsStream("kafka/localhost-producer.properties"))
+    srcDataStream.map(data => data.toString).addSink {
+      sinkFunction = new FlinkKafkaProducer[String]("target-topic", new SimpleStringSchema(), new Properties())
+    }
+
+
+
+
+
+
+
+
+
+
+    //3. 启动
+    env.execute(this.getClass.getSimpleName)
+  }
+}
